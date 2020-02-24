43463e26ee65c5e015fd67ee8668b4818f49a238
Roshan bhattarai <mailme.roshanbhattarai@gmail.com>
2020-02-24 08:20:43 +0545

    passing data to the pushed page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,21 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget{
 
+  final String _imageUrl,_title;
+
+  ProductPage(this._imageUrl,this._title);
+
+  @override
   Widget build(BuildContext context){
     return Scaffold( // Scaffold widget creates a page. Scaffold is built in
       // widget shipped with material package we imported in line 1
       appBar: AppBar( // creates an appBar. AppBar is also a built in widget
         // shipped with material package we imported in line 1
-        title:Text('Product Detail'), // adds text in the appBar
+        title:Text(_title), // adds text in the appBar
       ),
-      body:Column(children:<Widget>[
-        Text('on the product page'),
-        RaisedButton(
-          child:Text('BACK'),
-          onPressed: ()=>Navigator.pop(context),
-        )
-      ],
+      body:Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children:<Widget>[
+              Image.asset(_imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child:Text(_title),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child:RaisedButton(
+                        color: Theme.of(context).accentColor,
+                        child:Text('BACK'),
+                        onPressed: ()=>Navigator.pop(context),
+              ),
+              ),
+          ],
       ),
     );
 
