41202c721627777a2b816f3f3ab99ae6bfe27822
Roshan bhattarai <mailme.roshanbhattarai@gmail.com>
2020-02-24 22:09:05 +0545

    manually handling the back button in appBar

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:async'; // future is imported from this library
 
 class ProductPage extends StatelessWidget{
 
@@ -9,31 +10,38 @@ class ProductPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold( // Scaffold widget creates a page. Scaffold is built in
-      // widget shipped with material package we imported in line 1
-      appBar: AppBar( // creates an appBar. AppBar is also a built in widget
-        // shipped with material package we imported in line 1
-        title:Text(_title), // adds text in the appBar
-      ),
-      body:Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children:<Widget>[
-              Image.asset(_imageUrl),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child:Text(_title),
+    return WillPopScope(// listens for the back button being pressed
+        onWillPop: (){
+          Navigator.pop(context,false);// here you can return any boolean value, true if you want to delete the product 
+          return Future.value(false);// returning true here will trigger another pop action which will try to pop home page
+          // and the app crashes
+        },
+        child:Scaffold( // Scaffold widget creates a page. Scaffold is built in
+              // widget shipped with material package we imported in line 1
+              appBar: AppBar( // creates an appBar. AppBar is also a built in widget
+                // shipped with material package we imported in line 1
+                title:Text(_title), // adds text in the appBar
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child:RaisedButton(
-                        color: Theme.of(context).accentColor,
-                        child:Text('DELETE'),
-                        onPressed: ()=>Navigator.pop(context, true),
+              body:Column(
+                 // mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:<Widget>[
+                      Image.asset(_imageUrl),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        child:Text(_title),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        child:RaisedButton(
+                                color: Theme.of(context).accentColor,
+                                child:Text('DELETE'),
+                                onPressed: ()=>Navigator.pop(context, true),
+                      ),
+                      ),
+                  ],
               ),
-              ),
-          ],
-      ),
+            ),
     );
 
   }
