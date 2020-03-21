d58165abe4aa8e2aaf99b0b3cfd6a83369852edd
Roshan bhattarai <mailme.roshanbhattarai@gmail.com>
2020-03-21 19:50:27 +0545

    validation added

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -14,11 +14,18 @@ class ProductCreatePage extends StatefulWidget {
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
   final Map<String, dynamic> _product = {'image': 'assets/food.jpg'};
+  final GlobalKey <FormState> _formKey = GlobalKey<FormState>();
 
-  Widget _buildTitleTextField() {
-    return TextField(
+  Widget _buildTitleTextFormField() {
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
-      onChanged: (String value) {
+      validator: (String value){
+        if(value.isEmpty || value.length<5){
+            return 'Title is Required and contain at least five characters'; // error message
+        }
+        return null; // validation succeeded
+      },
+      onSaved: (String value) {
         setState(() {
           _product['title'] = value;
         });
@@ -26,23 +33,36 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     );
   }
 
-  Widget _buildPriceTextField() {
-    return TextField(
+  Widget _buildPriceTextFormField() {
+    return TextFormField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(labelText: 'Product Price'),
-      onChanged: (String value) {
+      validator: (String value){
+        
+        if( value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:[.,]\d+)?$').hasMatch(value)){ //also allows 9,99 numbers
+          return 'Price is Required and must be a number'; // error message
+        }
+        return null; // validation succeeded
+      },
+      onSaved: (String value) {
         setState(() {
-          _product['price'] = double.parse(value);
+          _product['price'] = double.parse(value.replaceFirst(RegExp(r','),'.'));
         });
       },
     );
   }
 
-  Widget _buildDescriptionTextField() {
-    return TextField(
+  Widget _buildDescriptionTextFormField() {
+    return TextFormField(
       maxLines: 5,
       decoration: InputDecoration(labelText: 'Product Description'),
-      onChanged: (String value) {
+      validator: (String value){
+        if(value.isEmpty || value.length < 5){
+          return 'Description is Required and contain at least five characters'; // error message
+        }
+        return null; // validation succeeded
+      },
+      onSaved: (String value) {
         setState(() {
           _product['description'] = value;
         });
@@ -55,46 +75,53 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double deviceWidth =
         MediaQuery.of(context).size.width; // getting the device current width
 
-    final double finalDeviceWidth =
-        deviceWidth > 768.0 ? 450.0 : deviceWidth * 0.95;
+    final double finalDeviceWidth = deviceWidth > 768.0 ? 450.0 : deviceWidth * 0.95;
     final double finalPadding = deviceWidth - finalDeviceWidth;
 
     return Container(
       margin: EdgeInsets.all(10.0),
-      child: ListView(
-        // listview items always take the full available space by default
-        padding: EdgeInsets.symmetric(
-          horizontal: finalPadding / 2,
-        ), // adding padding restricts the listView from using full width
-        children: <Widget>[
-          _buildTitleTextField(),
-          _buildDescriptionTextField(),
-          _buildPriceTextField(),
-          SizedBox(
-            height: 10.0,
-          ), // doesnot render anything just occupies space
-//          RaisedButton(
-//            child: Text('Save'),
-//            textColor: Colors.white,
-//            onPressed: () {
-//              widget.addProduct(_product);
-//              Navigator.pushReplacementNamed(context, '/products'); // pressing back button will not navigate here
-//             },
-//          ),
-          Center(
-            child: GestureDetector(
-              onTap: (){
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          // listview items always take the full available space by default
+          padding: EdgeInsets.symmetric(
+            horizontal: finalPadding / 2,
+          ), // adding padding restricts the listView from using full width
+          children: <Widget>[
+            _buildTitleTextFormField(),
+            _buildDescriptionTextFormField(),
+            _buildPriceTextFormField(),
+            SizedBox(
+              height: 10.0,
+            ), // doesnot render anything just occupies space
+            RaisedButton(
+              child: Text('Save'),
+              textColor: Colors.white,
+              onPressed: () {
+                if(!_formKey.currentState.validate()){ //validator function runs at this point and then only shows error
+                  return; // in case of false donot run below code
+                }
+                _formKey.currentState.save(); // when we call the save method on the state returned by the global key,
+                // the onSaved() methods are fired on each Form Fields
                 widget.addProduct(_product);
-              Navigator.pushReplacementNamed(context, '/products');
+                Navigator.pushReplacementNamed(context,'/products'); // pressing back button will not navigate here
               },
-              child: Container(
-                color: Colors.green,
-                padding: EdgeInsets.all(10.0),
-                child: Text('Save'),
-              ),
             ),
-          ),
-        ],
+//          Center(
+//            child: GestureDetector(
+//              onTap: (){
+//                widget.addProduct(_product);
+//              Navigator.pushReplacementNamed(context, '/products');
+//              },
+//              child: Container(
+//                color: Colors.green,
+//                padding: EdgeInsets.all(10.0),
+//                child: Text('Save'),
+//              ),
+//            ),
+//          ),
+          ],
+        ),
       ),
     );
   }
